c351d5a9383912578036e6b1300d19977ebcae17
Walter Futch <walter.futch@gmail.com>
2019-10-15 17:28:11 -0400

    array-methods.js complete
    Completed array-methods.js challenges.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,7 +41,11 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+const length = getLength(items.length);
+console.log(length);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
